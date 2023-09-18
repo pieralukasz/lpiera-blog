e985c5deddbe2887af873cefe4a143cdcc46218c
Lucas Piera <l.piera@protonmail.com>
2023-09-18 16:19:31 +0200

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "l.piera",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avataaars.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using notion.",
     email: "pieralukasz@gmail.com  ",
